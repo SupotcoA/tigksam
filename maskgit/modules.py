d8bdc8a24314c619a84fdbb218ffdd25c43f858c
SupotcoA <152614457+SupotcoA@users.noreply.github.com>
2024-04-25 19:04:25 +0800

    20240425_01

--- maskgit/modules.py
+++ maskgit/modules.py
@@ -71,6 +71,8 @@ class ResBlock(nn.Module):
                                             self.in_channels) \
                                         // res_bottle_neck_factor
             self.bottle_neck_channels = max(32, self.bottle_neck_channels)
+        if self.bottle_neck_channels >= 128 and divmod(self.bottle_neck_channels,32)[1]!=0:
+            self.bottle_neck_channels = (1+int(self.bottle_neck_channels/32))*32
 
         self.norm1 = nn.GroupNorm(num_groups=min(32, in_channels//4),
                                   num_channels=in_channels,
